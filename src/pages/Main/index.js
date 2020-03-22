9223e4b1e761e3ecee1d728304e7ca3018fecff7
Felipe Oliveira Saraiva <saraivafelipe27@gmail.com>
2020-03-22 13:15:53 -0300

    chore: Correção do bug das rotas.

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -86,7 +86,7 @@ export default class Main extends Component {
           {repositories.map(repository => (
             <li key={repository.name}>
               <span>{repository.name}</span>
-              <Link to={`/repository${encodeURIComponent(repository.name)}`}>
+              <Link to={`/repository/${encodeURIComponent(repository.name)}`}>
                 Detalhes
               </Link>
             </li>
--- src/routes.js
+++ src/routes.js
@@ -1,5 +1,4 @@
 import React from 'react';
-
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
 import Main from './pages/Main';
