ea43892984a7c506aa437acd54077bbdf7440a7a
Felipe Oliveira Saraiva <saraivafelipe27@gmail.com>
2020-03-22 17:40:23 -0300

    chore: exibição do repositório e navegação concluidas;

--- src/pages/Repository/index.js
+++ src/pages/Repository/index.js
@@ -1,9 +1,10 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import api from '../../services/api';
 
 import Container from '../../components/Container';
-import { Loading } from './styles';
+import { Loading, Owner } from './styles';
 
 export default class Repository extends Component {
   static propTypes = {
@@ -49,6 +50,15 @@ export default class Repository extends Component {
       return <Loading>Carregando...</Loading>;
     }
 
-    return <Container>Repository</Container>;
+    return (
+      <Container>
+        <Owner>
+          <Link to="/">Voltar aos repositórios</Link>
+          <img src={repository.owner.avatar_url} alt={repository.owner.login} />
+          <h1>{repository.name}</h1>
+          <p>{repository.description}</p>
+        </Owner>
+      </Container>
+    );
   }
 }
--- src/pages/Repository/styles.js
+++ src/pages/Repository/styles.js
@@ -9,3 +9,34 @@ export const Loading = styled.div`
   align-items: center;
   height: 100vh;
 `;
+
+export const Owner = styled.header`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  a {
+    color: #43b1bf;
+    font-size: 16px;
+    text-decoration: none;
+  }
+
+  img {
+    width: 100px;
+    margin-top: 20px;
+  }
+
+  h1 {
+    font-size: 25px;
+    margin-top: 14px;
+  }
+
+  p {
+    margin: 5px;
+    font-size: 14px;
+    color: #777;
+    line-height: 1.4;
+    text-align: center;
+    max-width: 400px;
+  }
+`;
